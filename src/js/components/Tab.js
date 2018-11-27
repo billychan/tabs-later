be8da4b9d8ba56904e50b76e72c679b4b856c9e5
Billy Chan <billyconnect@gmail.com>
2018-11-27 21:59:38 +0800

    TL-3 add checkbox to each tab item

--- src/js/components/Tab.js
+++ src/js/components/Tab.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Checkbox } from "@blueprintjs/core";
 
 export default function Tab({ tab }) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleChange({ checked }) {
+    setIsChecked(checked);
+  }
+
   return (
-    <li className="tab">
-      <div className="tab-favicon"><img src={tab.favIconUrl}></img></div>
-      <a href={tab.url}>{tab.title}</a>
+    <li className="tab flex-container">
+      <Checkbox className="tab-checkbox" onChange={handleChange} />
+      <span className="tab-favicon"><img src={tab.favIconUrl}></img></span>
+      <span className="tab-title">
+        <a href={tab.url} alt={tab.title}>{tab.title}</a>
+      </span>
     </li>
   )
 }
