5b4cdb54cca31ab9a16127076d1df093d5ed909e
Billy Chan <billyconnect@gmail.com>
2019-01-01 16:33:38 +0800

    TL-9 Organize panel components

---
+++ src/js/components/AddToListPanel.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { Classes,
+  Button,
+} from "@blueprintjs/core";
+import NewListPanel from './NewListPanel';
+
+const AddToListPanel = ({ openPanel }) =>
+  <div className='panel-content'>
+    <section className='main-section'>
+      content
+    </section>
+    <section className='actions'>
+      <Button text='New List' onClick={() => {
+        openPanel({
+          component: NewListPanel,
+          title: 'New List'
+        })
+      }} />
+      <Button text='Cancel' className={classNames(Classes.POPOVER_DISMISS, 'secondary-btn')} />
+      <Button text='Done' intent="primary"/>
+    </section>
+  </div>
+
+AddToListPanel.propTypes = {
+  openPanel: PropTypes.func.isRequired
+}
+
+export default AddToListPanel;
--- src/js/components/BulkAddToList.js
+++ src/js/components/BulkAddToList.js
@@ -1,36 +1,10 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import {
-  Classes,
   Button,
   PanelStack,
   Popover
 } from "@blueprintjs/core";
-
-const NewListPannel = () =>
-  <p>Add New List</p>
-
-const AddToListPannel = ({ openPanel }) =>
-  <div className="add-to-list-panel">
-    <section className='main-section'>
-      content
-    </section>
-    <section className='actions'>
-      <Button text='New List' onClick={() => {
-        openPanel({
-          component: NewListPannel,
-          title: 'New List'
-        })
-      }} />
-      <Button text='Cancel' className={classNames(Classes.POPOVER_DISMISS, 'cancle')} />
-      <Button text='Done' intent="primary"/>
-    </section>
-  </div>
-
-AddToListPannel.propTypes = {
-  openPanel: PropTypes.func.isRequired
-}
+import AddToListPanel from './AddToListPanel';
 
 const BulkAddToList = () =>
   <Popover>
@@ -40,8 +14,7 @@ const BulkAddToList = () =>
     />
     <div className="add-to-list-box">
       <PanelStack
-        initialPanel={{ component: AddToListPannel, title: 'Add To List' }}
-        className='add-to-list-content'
+        initialPanel={{ component: AddToListPanel, title: 'Add To List' }}
       />
     </div>
   </Popover>
