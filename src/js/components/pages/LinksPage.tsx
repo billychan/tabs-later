fd7ebfcc5142d458a5da7d69ba3ff89794a07feb
Billy Chan <billyatworking@gmail.com>
2019-08-16 22:54:17 +0800

    TL-47 Fix search updating and selection issue in links page

--- src/js/components/pages/LinksPage.tsx
+++ src/js/components/pages/LinksPage.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { without, map } from 'lodash';
 import { cold } from 'react-hot-loader';
 
 import { calculateCheckedStatus } from 'common/helpers';
@@ -14,23 +15,6 @@ import SelectedItemsHints from 'components/elements/SelectedItemsHints';
 
 const { useState, useEffect } = React;
 
-const selectLink = (link: TabsLater.Link, toSelect = true) => ({ ...link, checked: toSelect });
-
-const mergeLinksWithCheckedState = (links: TabsLater.Link[], localLinks: TabsLater.Link[]) => (
-  links.map((link) => {
-    const localLink = localLinks.find(item => item.id === link.id);
-    if (localLink) {
-      return {
-        ...link,
-        checked: localLink.checked,
-      };
-    }
-    return link;
-  })
-);
-
-const getCheckedLinksCount = (links: TabsLater.Link[]) => links.filter(link => link.checked).length;
-
 interface LinksPageProps {
   links: TabsLater.Link[];
   renderBulkOperations: TabsLater.Renderer;
@@ -45,24 +29,39 @@ const LinksPage = ({
   className,
 }: LinksPageProps) => {
   const linksCount = links.length;
+
+  const [checkedIds, setCheckedIds] = useState([]);
+
   const [visibleLinks, setVisibleLinks] = useState(links);
-  // The links prop may change, sync with it with local state
+
+  // Flag to decide if the links should be updated. In normal case they should, but if in search
+  // then the title/urls are all changed to highlight and they should not be updated from outside.
+  const [hasQuery, setHasQuery] = useState(false);
+
   useEffect(() => {
-    setVisibleLinks(mergeLinksWithCheckedState(links, visibleLinks));
+    if (!hasQuery) {
+      setVisibleLinks(links);
+    }
   }, [links]);
+
   return (
     <section className={classNames('LinksPage', className)}>
       <section className="flex pb-3 border-b border-gray-300 items-center">
         <BulkCheck
-          checkedStatus={calculateCheckedStatus(visibleLinks.length, getCheckedLinksCount(visibleLinks))}
+          checkedStatus={
+            calculateCheckedStatus(visibleLinks.length, checkedIds.length)
+          }
           onChange={(checked) => {
-            setVisibleLinks(visibleLinks.map((link: TabsLater.Link) => selectLink(link, checked)))
+            setCheckedIds(
+              checked ? map(visibleLinks, 'id') : []
+            )
           }}
         />
         <SearchInput
           onSearch={(query) => {
+            setHasQuery(!!query);
             if (query) {
-              setVisibleLinks(searchLinks(visibleLinks, query));
+              setVisibleLinks(searchLinks(links, query));
             } else {
               setVisibleLinks(links);
             }
@@ -82,13 +81,11 @@ const LinksPage = ({
               title={link.titleHighlighted || link.title}
               url={link.urlHighlighted || link.url}
               key={link.id}
-              checked={link.checked}
-              onChange={(_id, checked) => {
-                setVisibleLinks(visibleLinks.map(linkItem => (
-                  link.id === linkItem.id
-                    ? { ...link, checked }
-                    : linkItem
-                )));
+              checked={checkedIds.includes(link.id)}
+              onChange={(id, checked) => {
+                setCheckedIds(
+                  checked ? checkedIds.concat(id) : without(checkedIds, id)
+                )
               }}
             >
               { renderItemOperations({ link }) }
@@ -99,7 +96,7 @@ const LinksPage = ({
       <Footer>
         <span className="text-xs text-gray-600">
           <SelectedItemsHints
-            selectedCount={getCheckedLinksCount(visibleLinks)}
+            selectedCount={checkedIds.length}
             totalCount={linksCount}
           />
         </span>
